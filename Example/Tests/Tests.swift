125fc3ceca42274b9a1e309edc3a5d259ec6508e
Stephan Lerner <stephanlerner@icloud.com>
2019-02-12 16:34:47 +0100

    fixed swift versions and deployment target

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,11 +1,13 @@
 import XCTest
 import SwiftArchitekkt
+import CoreArchitekkt
 
 class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        print("test")
     }
     
     override func tearDown() {
