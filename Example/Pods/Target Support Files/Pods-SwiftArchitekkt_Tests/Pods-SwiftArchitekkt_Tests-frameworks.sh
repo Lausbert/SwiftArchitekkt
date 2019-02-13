d2009765883ce8fe367530c4335f1e8f8a1d0dec
Stephan Lerner <stephanlerner@icloud.com>
2019-02-13 12:40:09 +0100

    removed old files

--- Example/Pods/Target Support Files/Pods-SwiftArchitekkt_Tests/Pods-SwiftArchitekkt_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-SwiftArchitekkt_Tests/Pods-SwiftArchitekkt_Tests-frameworks.sh
@@ -154,11 +154,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CoreArchitekkt/CoreArchitekkt.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftArchitekkt/SwiftArchitekkt.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CoreArchitekkt/CoreArchitekkt.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftArchitekkt/SwiftArchitekkt.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
