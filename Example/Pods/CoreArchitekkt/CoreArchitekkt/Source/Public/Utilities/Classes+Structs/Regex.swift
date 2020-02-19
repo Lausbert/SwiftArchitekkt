e48903731a771077a1f5ddc7e4e87f3a17ddd6f5
Stephan Lerner <Stephanlerner@icloud.com>
2020-02-19 19:22:43 +0100

    did run pod update

--- Example/Pods/CoreArchitekkt/CoreArchitekkt/Source/Public/Utilities/Classes+Structs/Regex.swift
+++ Example/Pods/CoreArchitekkt/CoreArchitekkt/Source/Public/Utilities/Classes+Structs/Regex.swift
@@ -19,31 +19,43 @@ public struct Regex {
         let captureGroup: Int
         let string: String
         let range: Range<String.Index>
+        let regex: String
+        let text: String
+    }
+
+    public static func getMatchingStrings(for regex: StaticString, text: String, captureGroup: Int) -> [Result] {
+        return getMatchingStrings(for: regex, text: text, captureGroups: captureGroup).flatMap { $0.results }
+    }
+
+    public static func getMatchingStrings(for regex: String, text: String, captureGroup: Int) throws -> [Result] {
+        return try getMatchingStrings(for: regex, text: text, captureGroups: captureGroup).flatMap { $0.results }
     }
 
     public static func getMatchingStrings(for regex: StaticString, text: String, captureGroups: Int...) -> [CombinedResult] {
         let matches = try! getMatches(for: "\(regex)", text: text)
         return matches.map { match in
-            getMachingStrings(for: match, text: text, captureGroups: captureGroups)
+            getMachingStrings(for: match, regex: "\(regex)", text: text, captureGroups: captureGroups)
         }
     }
 
     public static func getMatchingStrings(for regex: String, text: String, captureGroups: Int...) throws -> [CombinedResult] {
         let matches = try getMatches(for: regex, text: text)
         return matches.map { match in
-            getMachingStrings(for: match, text: text, captureGroups: captureGroups)
+            getMachingStrings(for: match, regex: regex, text: text, captureGroups: captureGroups)
         }
     }
 
     // MARK: - Private -
 
-    private static func getMachingStrings(for match: NSTextCheckingResult, text: String, captureGroups: [Int]) -> CombinedResult {
+    private static func getMachingStrings(for match: NSTextCheckingResult, regex: String, text: String, captureGroups: [Int]) -> CombinedResult {
         let results = captureGroups
             .compactMap { captureGroup in
                 Result(
                     captureGroup: captureGroup,
                     string: Range(match.range(at: captureGroup), in: text).map({String(text[$0])})!,
-                    range: Range(match.range(at: captureGroup), in: text)!
+                    range: Range(match.range(at: captureGroup), in: text)!,
+                    regex: regex,
+                    text: text
                 )
             }
         return CombinedResult(results: results)
