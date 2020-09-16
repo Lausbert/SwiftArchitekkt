08f899a81d231b493aaa8c52bebf2396216b4190
Stephan Lerner <Stephanlerner@icloud.com>
2020-09-16 07:53:02 +0200

    updated deployment version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftArchitekkt",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         .library(
